11719f12042a7a20f012a0a17b491ee448eda959
Denys Bonfim <denysbonfim1@gmail.com>
2026-03-09 15:31:15 +0000

    setup: malloc's finish

---
+++ srcs/ft_calloc.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	size_t	total;
+	char	*pbr;
+
+	total = nmemb * size;
+	pbr = malloc(total);
+	if (pbr == NULL)
+	{
+		return (NULL);
+	}
+	ft_memset(pbr,0,total);
+	return (pbr);
+}
